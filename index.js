638007834238fc9a10816896dbd9026740f48fea
denismnjima <denismnjima@gmail.com>
2024-07-19 16:31:40 +0300

    solved question

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left, 10);
+  
+    // Move left only if it's greater than 0
+    if (left > 0) {
+      dodger.style.left = `${left - 4}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left, 10);
+  
+    // Move right only if it's less than 360
+    if (left < 360) {
+      dodger.style.left = `${left + 4}px`;
+    }
+  }
+  
